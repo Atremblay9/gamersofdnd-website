5101026205290248fc54032728fc75a811a61eb9
Atremblay9 <atremblay9@nait.ca>
2025-01-05 22:24:52 -0700

    basic layout!

--- app/page.js
+++ app/page.js
@@ -6,10 +6,19 @@ export default function Home() {
       <div className="hero-container">
         <p className="login">Admin</p>
         <h1>Gamers Of Dungeons and Dragons</h1>
-        
+      </div>
+      <div className="description">
+        <p>This is a Website created for Nait's Gamers of Dungeons and Dragons club. It's purpose is to support the club and aid in function.</p>
+      </div>
+      <div className="pills">
+        <h3>Current Games Component</h3>
+        <h3>Request for Game Component</h3>
+      </div>
+      <div className="inventory">
+        <h3>Search current Inventory component</h3>
+        <h3>Add to Inventory component</h3>
         
       </div>
-      <p>This is the home page</p>
     </div>
   );
 }
